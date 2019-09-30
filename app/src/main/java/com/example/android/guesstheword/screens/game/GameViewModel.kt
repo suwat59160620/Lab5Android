cfe590b39fa47ccac9e861dbc373a5e7beca9230
bancha <ronnybcjt2311@gmail.com>
2019-09-30 19:46:41 +0700

    addLiveData

---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,85 @@
+package com.example.android.guesstheword.screens.game
+
+import androidx.lifecycle.ViewModel
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+
+
+class GameViewModel : ViewModel() {
+
+    // The current word
+    val word = MutableLiveData<String>()
+    // The current score
+    val score = MutableLiveData<Int>()
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    init {
+        Log.i("GameViewModel", "GameViewModel created!")
+        word.value = ""
+        score.value = 0
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel", "GameViewModel destroyed!")
+    }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+     fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /** Methods for buttons presses **/
+
+     fun onSkip() {
+        if (!wordList.isEmpty()) {
+            score.value = (score.value)?.minus(1)
+        }
+        nextWord()
+    }
+
+     fun onCorrect() {
+        if (!wordList.isEmpty()) {
+            score.value = (score.value)?.plus(1)
+        }
+        nextWord()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+     fun nextWord() {
+        if (!wordList.isEmpty()) {
+            //Select and remove a word from the list
+            word.value = wordList.removeAt(0)
+        }
+
+    }
+}
